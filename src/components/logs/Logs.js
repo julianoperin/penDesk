d4165afe081b8711e750f900048077d1a62ea42e
‘Juliano <julianoperins@gmail.com>
2021-03-18 19:55:19 -0400

    finished the logs

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import LogItem from "./LogItem";
+
 const Logs = () => {
   const [logs, setLogs] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -23,16 +25,14 @@ const Logs = () => {
   }
 
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className="center">No logs to show</p>
       ) : (
-        logs.map((log) => {
-          return <li>{log.message}</li>;
-        })
+        logs.map((log) => <LogItem key={log.id} log={log} />)
       )}
     </ul>
   );
